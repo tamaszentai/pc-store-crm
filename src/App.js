a61c004a54270bbd894be1dfdb68a0c8708ab25e
Tamas Zentai <zentaitamas@gmail.com>
2020-10-31 12:22:37 +0000

    implementing redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from "redux";
 
 import Header from "./Header";
 import LoginPage from "./LoginPage";
@@ -7,11 +9,15 @@ import Products from "./Products";
 import Dashboard from "./Dashboard";
 import Orders from "./Orders";
 
+
+const store = createStore(() => [], {}, applyMiddleware());
+
 function App() {
   const token = { email: "admin@pcstore.com", password: "admin" };
   localStorage.setItem('token', JSON.stringify(token));
 
   return (
+    <Provider store={store}>
     <div className="App">
       <Router>
       <Header />
@@ -23,6 +29,7 @@ function App() {
       </Switch>
       </Router>
     </div>
+    </Provider>
   );
 }
 
